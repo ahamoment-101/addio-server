30901536b5d05de17d3a12203490f83a8513155b
darren-zhang <darrenlopez@163.com>
2024-11-13 00:01:55 +0800

    create serverless

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 // //localhost
-const url = 'mongodb://localhost:27017' ; 
-const dbName = 'Addio'
+// const url = 'mongodb://localhost:27017' ; 
+// const dbName = 'Addio'
 
 // Serverless
-// const url = 'mongodb+srv://darrenlopezhr:darrenlopez565@cluster0.n3hlo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' ; 
-// const dbName = 'Addio'
+const url = 'mongodb+srv://darrenlopezhr:darrenlopez565@cluster0.n3hlo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' ; 
+const dbName = 'Addio'
 
 //开始链接数据库
 mongoose.connect(`${url}/${dbName}`, {
