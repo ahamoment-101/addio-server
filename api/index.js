c45be4a8662a7807c75cbf608fe262724f153505
darren-zhang <darrenlopez@163.com>
2024-11-13 00:55:24 +0800

    Fix Vercel deployment issues

--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
 // api/index.js
-const app = require('../src/app');
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const cors = require('@koa/cors');
+const app = new Koa();
+
+// 加载中间件
+require('dotenv').config();
+app.use(bodyParser());
+app.use(cors());
+
+// 加载路由
+const userInput = require('../src/routes/userInput');
+const todoList = require('../src/routes/todoList');
+
+app.use(userInput.routes()).use(userInput.allowedMethods());
+app.use(todoList.routes()).use(todoList.allowedMethods());
+
+// 导出 app 实例
 module.exports = app;
--- src/db/index.js
+++ src/db/index.js
@@ -1,25 +1,38 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-// //localhost
-const url = 'mongodb://localhost:27017' ; 
-const dbName = 'Addio'
-
-// Serverless
-// const url = 'mongodb+srv://darrenlopezhr:darrenlopez565@cluster0.n3hlo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' ; 
+// // //localhost
+// const url = 'mongodb://localhost:27017' ; 
 // const dbName = 'Addio'
 
-//开始链接数据库
-mongoose.connect(`${url}/${dbName}`, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: truea
+// // Serverless
+// // const url = 'mongodb+srv://darrenlopezhr:darrenlopez565@cluster0.n3hlo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0' ; 
+// // const dbName = 'Addio'
+
+// //开始链接数据库
+// mongoose.connect(`${url}/${dbName}`, {
+//     // useNewUrlParser: true,
+//     // useUnifiedTopology: truea
     
-});
+// });
 
-//获取链接对象
-const conn = mongoose.connection;
+// //获取链接对象
+// const conn = mongoose.connection;
 
-conn.on('error' , err =>{
-    console.log('mongodb 链接出错', err);
-})
+// conn.on('error' , err =>{
+//     console.log('mongodb 链接出错', err);
+// })
 
-module.exports = mongoose
+// module.exports = mongoose
+
+// vercel版本
+// src/db/index.js
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
+const dbName = 'Addio';
+
+mongoose.connect(`${url}/${dbName}`)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = mongoose;
